0ec47e18a0873bb834bd895550bcb80fa32307cd
DonatoRM <donatoramos@hotmail.com>
2022-10-28 21:56:16 +0200

    Apartado 2 finalizado

---
+++ apartado2/ejercicio2.js
@@ -0,0 +1,18 @@
+/* Declaramos la función anónima ejecutar, que es la que se ejecutará con el evento onload() del 
+body del html. Se declara como constante global. */
+const ejecutar = () => {
+  // * caja es la variable local que utilizaremos para hacer referencia al main>section>ul#inicio del html
+  let caja = document.getElementById("inicio");
+  // * Con las dos constantes locales siguientes establecemos los parámetros de la operación
+  const MULTIPLICANDO = 40;
+  const MULTIPLICADOR = 4;
+  /* El número de lugares que tenemos que desplazar se obtiene de la siguiente forma:
+  4 = 2^X -> ln 4 = ln 2^X -> ln 4 = X ln 2 -> X = (ln 4 / ln 2).
+  Para el cálculo del desplazamientoLugares utilizaremos la función Math.log  */
+  let desplazamientoLugares = Math.log(MULTIPLICADOR) / Math.log(2);
+  /* Al desplazar los bits del MULTIPLICANDO hacia la izquierda, se va multiplicando por 2 y, a la derecha
+  se van añadiendo ceros con << */
+  let producto = MULTIPLICANDO << desplazamientoLugares;
+  // * Se envía el resultado a main>section#inicio del fichero ejercicio1.html
+  caja.innerHTML = "<p>" + MULTIPLICANDO + " x " + MULTIPLICADOR + " = " + producto + "</p>";
+};
